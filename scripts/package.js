1bca89d63b48dcb7873fee3b7d3266bf1f290c9d
Owen Kieffer-Jones <okj579@gmail.com>
2024-09-04 06:00:52 +0200

    fix: support older node versions in package script

--- scripts/package.js
+++ scripts/package.js
@@ -1,8 +1,9 @@
 import { join, relative, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { createWriteStream } from "node:fs";
 import archiver from "archiver";
 
-const rootDir = resolve(import.meta.dirname, '..');
+const rootDir = resolve(fileURLToPath(import.meta.url), "../..");
 const distDir = resolve(rootDir, "dist");
 const zipFile = join(rootDir, "dist.zip");
 
@@ -13,5 +14,5 @@ archive.pipe(output);
 archive.directory(distDir, false);
 await archive.finalize();
 
-console.log(`${relative(rootDir, zipFile)} created`)
-console.log(`${archive.pointer()} / ${13*1024} total bytes`);
+console.log(`${relative(rootDir, zipFile)} created`);
+console.log(`${archive.pointer()} / ${13 * 1024} total bytes`);
