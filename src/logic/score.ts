bd868f8d32aa4a3b32d1db517058d3abd19cf5a0
mxlle <almut.m.f@gmail.com>
2024-09-04 00:14:51 -0500

    refactor: score code

--- src/logic/score.ts
+++ src/logic/score.ts
@@ -1,17 +1,18 @@
 import { PlacedPerson } from "../types";
-import { getHappyStats } from "./checks";
 import { globals } from "../globals";
 
 export function calculateScore(persons: PlacedPerson[], moves: number): number {
-  const { happyGuests, totalGuests } = getHappyStats(persons);
-
-  if (!totalGuests || !globals.metaData) {
+  if (!persons.length || !globals.metaData) {
     return 0;
   }
 
-  const _progressModifier = Math.pow(happyGuests / totalGuests, 1);
-  const movesScore =
-    1 - Math.min(1, Math.max(moves - globals.metaData.minMoves, 0) / (globals.metaData.maxMoves - globals.metaData.minMoves));
+  const { minMoves, maxMoves } = globals.metaData;
+  const moveSpan = Math.max(maxMoves - minMoves, 1);
+  const diffToMin = moves - minMoves;
+
+  let movesScore = 1 - diffToMin / moveSpan;
+  movesScore = Math.min(movesScore, 1);
+  movesScore = Math.max(movesScore, 0);
 
   return Math.round(movesScore * 9999);
 }
