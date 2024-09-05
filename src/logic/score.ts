33410faf8455fc83c8116a58019a78370977fb85
mxlle <almut.m.f@gmail.com>
2024-09-04 22:09:19 -0500

    fix: grace score

--- src/logic/score.ts
+++ src/logic/score.ts
@@ -8,9 +8,13 @@ export function calculateScore(persons: PlacedPerson[], moves: number): number {
 
   const { minMoves, maxMoves } = globals.metaData;
   const moveSpan = Math.max(maxMoves - minMoves, 1);
-  const diffToMin = moves - minMoves;
+  const diffToMin = Math.max(0, moves - minMoves);
+  const diffToMax = Math.max(0, moves - maxMoves);
+  const linearRegular = Math.min(1, diffToMin / moveSpan);
+  // also still get some points for too much moves
+  const linearFail = Math.min(1, diffToMax / (3 * moveSpan));
 
-  let movesScore = 1 - diffToMin / moveSpan;
+  let movesScore = 1 - 0.8 * linearRegular - 0.2 * linearFail;
   movesScore = Math.min(movesScore, 1);
   movesScore = Math.max(movesScore, 0);
 
